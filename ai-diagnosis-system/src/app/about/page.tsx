d26299f2a702c1f6c666b04aeaa0de031a51310d
Luke Wang <lmwang@usc.edu>
2024-09-15 16:25:29 -0700

    [SETUP] - create website files

---
+++ ai-diagnosis-system/src/app/about/page.tsx
@@ -0,0 +1,21 @@
+export default function About() {
+    return <div >
+        <p className="flex justify-center border-2 border-black"> Each page directory requires a page.tsx file </p>
+
+        <br></br>
+
+        <p className="flex justify-center">
+            I will be using tailwind css for this project, you can learn more:
+            <a className="bg-[#fecaca]" href="https://tailwindcss.com/docs/installation">here</a>
+        </p>
+
+        <br></br>
+
+        <h3 className="flex justify-center text-4xl ">Frontend Architecture</h3>
+        <ul className="list-disc pl-10">
+            <li> React with Next.JS framework</li>
+            <li> Tailwind css</li>
+        </ul>
+
+        </div>
+  }2
